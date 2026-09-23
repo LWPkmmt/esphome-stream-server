91b82ff89e67f217f6891507757a3d9fe3a56598
lwp <wangpei.liu@coolkit.cn>
2026-09-23 11:55:35 +0800

    pass actual iovec count to writev()

--- components/stream_server/stream_server.cpp
+++ components/stream_server/stream_server.cpp
@@ -158,7 +158,9 @@ void StreamServerComponent::flush() {
         iov[0].iov_len = std::min(this->buf_head_ - client.position, this->buf_ahead(client.position));
         iov[1].iov_base = &this->buf_[0];
         iov[1].iov_len = this->buf_head_ - (client.position + iov[0].iov_len);
-        int iovcnt = iov[1].iov_len > 0 ? 2 : 1;
+        // Pass only populated iovecs to writev(). The second iovec is only
+        // needed when the ring buffer wraps.
+        const int iovcnt = iov[1].iov_len != 0 ? 2 : 1;
         if ((written = client.socket->writev(iov, iovcnt)) > 0) {
             client.position += written;
         } else if (written == 0 || errno == ECONNRESET || errno == ENOTCONN || errno == EPIPE) {
