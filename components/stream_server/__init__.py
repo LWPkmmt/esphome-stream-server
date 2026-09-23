c877e591491e68ffb682ce5c349db817ed110c1e
lwp <wangpei.liu@coolkit.cn>
2026-09-23 09:59:43 +0800

    delete parse_esphome_version

--- components/stream_server/__init__.py
+++ components/stream_server/__init__.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID, CONF_PORT, CONF_BUFFER_SIZE
-from esphome.util import parse_esphome_version
 
 # ESPHome doesn't know the Stream abstraction yet, so hardcode to use a UART for now.
 
